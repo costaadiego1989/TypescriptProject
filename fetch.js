dd6199ad5efefbd5d4ab3ed8a57be7737bbcc7b4
MVP Plus <equipe.mvpplus@gmail.com>
2024-10-06 14:27:30 -0300

    create: initial files and fetchData function

---
+++ fetch.js
@@ -0,0 +1,9 @@
+export default async function fetchData(url) {
+    try {
+    }
+    catch (error) {
+        if (error instanceof Error)
+            console.error("Error: " + error.message);
+        return null;
+    }
+}
---
+++ fetch.ts
@@ -0,0 +1,8 @@
+export default async function fetchData(url: string) {
+    try {
+        
+    } catch (error) {
+        if(error instanceof Error) console.error("Error: " + error.message);
+        return null;
+    }
+}

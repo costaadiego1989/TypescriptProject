7d1512a08b092db6e7dfb8fc4fce0c18386c2701
MVP Plus <equipe.mvpplus@gmail.com>
2024-10-06 14:31:07 -0300

    refactor: fetchData function

--- fetch.js
+++ fetch.js
@@ -1,5 +1,10 @@
 export default async function fetchData(url) {
     try {
+        const request = await fetch(`${url}`);
+        if (!request.ok)
+            throw new Error("Erro " + request.status);
+        const data = await request.json();
+        return data;
     }
     catch (error) {
         if (error instanceof Error)
